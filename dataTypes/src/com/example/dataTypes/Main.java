ec4550b380d8a6a1d840c77f496a6c8544ca868d
bulentbariskilic@gmail.com <bulentbariskilic@gmail.com>
2021-04-26 03:30:53 +0300

    Learned data types in Java.

---
+++ dataTypes/src/com/example/dataTypes/Main.java
@@ -0,0 +1,12 @@
+package com.example.dataTypes;
+
+public class Main {
+
+    public static void main(String[] args) {
+        byte kucukSayi = 127;
+        int sayi = 1234567898;
+        char karakter = 'A';
+        String metin = "ANKARA";
+        boolean dogruMu = false;
+    }
+}
