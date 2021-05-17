3bf37de3492a3cc416050977ce42c2fcff927122
bulentbariskilic@gmail.com <bulentbariskilic@gmail.com>
2021-05-17 22:59:48 +0300

    Arrays added.

---
+++ 10-arrays/src/com/example/arrays/Main.java
@@ -0,0 +1,22 @@
+package com.example.arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String[] ogrenciler = new String[4];
+        ogrenciler[0] = "Baris";
+        ogrenciler[1] = "Oznur";
+        ogrenciler[2] = "Ulas";
+        ogrenciler[3] = "Meryem";
+
+        for(int i = 0 ; i < ogrenciler.length; i++){
+            System.out.println(ogrenciler[i]);
+        }
+
+        System.out.println("=============================");
+
+        for(String ogrenci : ogrenciler){
+            System.out.println(ogrenci);
+        }
+    }
+}
