88520ec82c7b6f89be3a4de1019cbfeb78029599
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2022-06-13 01:26:21 +0300

    Mini project added.

---
+++ 16-miniProject/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    static public void main(String[] args) {
+        int number=28;
+        int total =0;
+
+        for (int i = 1; i<number; i++) {
+            if(number % i == 0) {
+                total += i;
+            }
+        }
+
+        if(total != number) return;
+
+        System.out.println(number +" Mükemmel sayi!");
+    }
+}
